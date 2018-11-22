edd244a0c595f0dcaba69bed525ded19aef3a0e0
iF4r3s <44950873+iF4r3s@users.noreply.github.com>
2018-11-22 18:10:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,124 +2,112 @@ const Discord = require('discord.js');
 const bot10 = new Discord.Client();
 
 bot10.on('ready', () => {
-  console.log(`Starting....`);
+  console.log('Logged in as S10-11-12')
 });
 
 
 bot10.on('message', message => {
-  if(message.content.startsWith(`f-d`)){
-    if(message.author.id !== "505101712839671838") return;
-    message.channel.send('#daily').then(msg => msg.delete(3000));
+  if(message.content.startsWith('f-d')){
+    message.channel.send('#daily')
   }
 });
 
 
 bot10.on('message', message => {
   if(message.content.startsWith(`f-r`)){
-    if(message.author.id !== "505101712839671838") return;
-    message.channel.send('#rep <@505101712839671838>').then(msg => msg.delete(3000));
+    message.channel.send(`#rep <@505101712839671838>`)
   }
 });
 
 
 bot10.on('ready', () => {
-  let c = bot10.guilds.get("507972785650401301").channels.find(c => c.id == "510732026949795852");
+  let ch = bot10.guilds.get("507972785650401301").channels.find(c => c.id == "510732026949795852");
   setInterval(function() {
-    c.send("10th Lover")
+    ch.send("11th Lover")
   }, 900);
 });
 
 
 bot10.on('message', message => {
-  if(message.author.bot) return;
+  let command = message.content.split(" ")[0];
   let args = message.content.split(" ").slice(1);
-  if(!args) return message.channel.send(`:angry: | What do you want us to say?`);
-  if(message.content.startsWith(`f10`)){
-    if(message.author.id !== "505101712839671838") return;
+  if(!args) return message.reply(`شو يدك امي تقول؟؟؟`);
+  if(command == "f10"){
     message.channel.send(args.join(" "))
   }
 });
 
 
-bot10.login(process.env.FB10)
-//////////////////////////////////////////////////////////
-
+bot10.login(process.env.FB10);
+////////////////////////
+////////////////////////
 const bot11 = new Discord.Client();
-
 bot11.on('message', message => {
-  if(message.content.startsWith(`f-d`)){
-    if(message.author.id !== "505101712839671838") return;
-    message.channel.send('#daily').then(msg => msg.delete(3000));
+  if(message.content.startsWith('f-d')){
+    message.channel.send('#daily')
   }
 });
 
 
 bot11.on('message', message => {
   if(message.content.startsWith(`f-r`)){
-    if(message.author.id !== "505101712839671838") return;
-    message.channel.send('#rep <@505101712839671838>').then(msg => msg.delete(3000));
+    message.channel.send(`#rep <@505101712839671838>`)
   }
 });
 
 
 bot11.on('ready', () => {
-  let c = bot11.guilds.get("507972785650401301").channels.find(c => c.id == "510732026949795852");
+  let ch = bot11.guilds.get("507972785650401301").channels.find(c => c.id == "510732026949795852");
   setInterval(function() {
-    c.send("11th Lover")
+    ch.send("11th Lover")
   }, 900);
 });
 
 
 bot11.on('message', message => {
-  if(message.author.bot) return;
+  let command = message.content.split(" ")[0];
   let args = message.content.split(" ").slice(1);
-  if(!args) return message.channel.send(`:angry: | What do you want us to say?`);
-  if(message.content.startsWith(`f11`)){
-    if(message.author.id !== "505101712839671838") return;
+  if(!args) return message.reply(`شو يدك امي تقول؟؟؟`);
+  if(command == "f11"){
     message.channel.send(args.join(" "))
   }
 });
 
 
-bot11.login(process.env.FB11)
-//////////////////////////////////////////////////////////
-
+bot11.login(process.env.FB11);
+////////////////////////
+////////////////////////
 const bot12 = new Discord.Client();
-
 bot12.on('message', message => {
-  if(message.content.startsWith(`f-d`)){
-    if(message.author.id !== "505101712839671838") return;
-    message.channel.send('#daily').then(msg => msg.delete(3000));
+  if(message.content.startsWith('f-d')){
+    message.channel.send('#daily')
   }
 });
 
 
 bot12.on('message', message => {
   if(message.content.startsWith(`f-r`)){
-    if(message.author.id !== "505101712839671838") return;
-    message.channel.send('#rep <@505101712839671838>').then(msg => msg.delete(3000));
+    message.channel.send(`#rep <@505101712839671838>`)
   }
 });
 
 
 bot12.on('ready', () => {
-  let c = bot12.guilds.get("507972785650401301").channels.find(c => c.id == "510732026949795852");
+  let ch = bot12.guilds.get("507972785650401301").channels.find(c => c.id == "510732026949795852");
   setInterval(function() {
-    c.send("12th Lover")
+    ch.send("12th Lover")
   }, 900);
 });
 
 
 bot12.on('message', message => {
-  if(message.author.bot) return;
+  let command = message.content.split(" ")[0];
   let args = message.content.split(" ").slice(1);
-  if(!args) return message.channel.send(`:angry: | What do you want us to say?`);
-  if(message.content.startsWith(`f12`)){
-    if(message.author.id !== "505101712839671838") return;
+  if(!args) return message.reply(`شو يدك امي تقول؟؟؟`);
+  if(command == "f12"){
     message.channel.send(args.join(" "))
   }
 });
 
 
-bot12.login(process.env.FB12)
-//////////////////////////////////////////////////////////
+bot12.login(process.env.FB12);
